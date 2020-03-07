ad75fa881df69e3d0f5928997cd83d86e9ed8cca
Steven Joseph Fontalvo Mora <programadoresudc@gmail.com>
2020-03-07 12:01:47 -0500

    rips I

--- src/public/js/miscrip.js
+++ src/public/js/miscrip.js
@@ -1,140 +1,140 @@
 // MEDICOS
 
-var paciente=[];
-var eps=[];
-var item=[];
-var listitem=[];
+var paciente = [];
+var eps = [];
+var item = [];
+var listitem = [];
 
-function consultarmedico(id){
-    $.ajax({
-      url: '/ajaxmedico',
-      type: 'POST',
-      datatype: 'json',
-      data:{      
-        id:id  
-      },
-      success: (data) => { 
+function consultarmedico(id) {
+  $.ajax({
+    url: '/ajaxmedico',
+    type: 'POST',
+    datatype: 'json',
+    data: {
+      id: id
+    },
+    success: (data) => {
 
-       $('#cedula').val(data[0].cedula);
-       $('#nombre').val(data[0].nombre);
-       $('#email').val(data[0].email);
-       $('#registro').val(data[0].registro);
-       $('#telefono').val(data[0].telefono);
+      $('#cedula').val(data[0].cedula);
+      $('#nombre').val(data[0].nombre);
+      $('#email').val(data[0].email);
+      $('#registro').val(data[0].registro);
+      $('#telefono').val(data[0].telefono);
 
+    }
+  });
+}
+function blurMedicoCedula() {
+  var id = $('#cedula').val();
+  $.ajax({
+    url: '/ajaxmedico',
+    type: 'POST',
+    datatype: 'json',
+    data: {
+      id: id
+    },
+    success: (data) => {
+      if (data[0]) {
+        $('#id').val(data[0].id);
+        $('#cedula').val(data[0].cedula);
+        $('#nombre').val(data[0].nombres);
+        $('#email').val(data[0].email);
+        $('#registro').val(data[0].registro);
+        $('#telefono').val(data[0].telefono);
+        $('#ingresar').css('visibility', 'hidden');
+        $('#actualizar').css('visibility', 'visible');
+      } else {
+        $('#nombre').val("");
+        $('#email').val("");
+        $('#registro').val("");
+        $('#telefono').val("");
       }
-    });  
-  }
-  function blurMedicoCedula(){
-    var id=$('#cedula').val();
-    $.ajax({
-      url: '/ajaxmedico',
-      type: 'POST',
-      datatype: 'json',
-      data:{      
-        id:id  
-      },
-      success: (data) => { 
-        if(data[0]){
-            $('#id').val(data[0].id);
-            $('#cedula').val(data[0].cedula);
-            $('#nombre').val(data[0].nombres);
-            $('#email').val(data[0].email);
-            $('#registro').val(data[0].registro);
-            $('#telefono').val(data[0].telefono);
-            $('#ingresar').css('visibility','hidden');
-            $('#actualizar').css('visibility','visible');
-        }else{           
-            $('#nombre').val("");
-            $('#email').val("");
-            $('#registro').val("");
-            $('#telefono').val("");
-        }      
-      }
-    });  
-  }
-  function actualizarMedico(){      
-    $.ajax({
-      url: '/actualizarmedico',
-      type: 'POST',
-      datatype: 'json',
-      data:{
-        id:$('#id').val(),
-        nombres:$('#nombre').val(),
-        email:$('#email').val(),
-        registro:$('#registro').val(),
-        telefono:$('#telefono').val() 
-      },
-      success: (data) => { 
-          console.log(data);
-          location.href = "/addmedicos";
-      }
-    });  
-  }
+    }
+  });
+}
+function actualizarMedico() {
+  $.ajax({
+    url: '/actualizarmedico',
+    type: 'POST',
+    datatype: 'json',
+    data: {
+      id: $('#id').val(),
+      nombres: $('#nombre').val(),
+      email: $('#email').val(),
+      registro: $('#registro').val(),
+      telefono: $('#telefono').val()
+    },
+    success: (data) => {
+      console.log(data);
+      location.href = "/addmedicos";
+    }
+  });
+}
 //   MEDICOS
 
 // --- SELECT ---
-function selectEntidad(){ 
+function selectEntidad() {
   console.log('Select Entidad');
   $.ajax({
     url: '/selectEntidad',
     type: 'POST',
-    datatype: 'json',   
-    success: (data) => { 
-      var cadena=`
+    datatype: 'json',
+    success: (data) => {
+      var cadena = `
       <select name="entidad" id="entidad" onchange="consultarEntidad();selectItems();" class="form-control form-control-sm">
       <option value="">Seleccionar Entidad</option>
       `;
       data.forEach(element => {
-        cadena+=` <option value="${element.nit}">${element.rsocial}</option>`;
+        cadena += ` <option value="${element.nit}">${element.rsocial}</option>`;
       });
-      cadena+=`</select>`;
+      cadena += `</select>`;
       $('.entidades').html(cadena);
     }
-  });  
+  });
 }
 
-function selectItems(){ 
+function selectItems() {
   console.log('Select Entidad');
-  var entidad=$('#entidad').val();
+  var entidad = $('#entidad').val();
   $.ajax({
     url: '/itemcups',
     type: 'POST',
     datatype: 'json',
-    data:{      
-      entidad:entidad  
-    },   
-    success: (data) => { 
-      var cadena=`
+    data: {
+      entidad: entidad
+    },
+    success: (data) => {
+      var cadena = `
       <select name="item" id="item" onchange="consultarItem();" class="form-control form-control-sm">
       <option value="">Seleccionar Item</option>`;
       data.forEach(element => {
-        cadena+=` <option value="${element.id}">${element.nombre}</option>`;
+        cadena += ` <option value="${element.id}">${element.nombre}</option>`;
       });
-      cadena+=`</select>`;
+      cadena += `</select>`;
       $('.items').html(cadena);
     }
-  });  
+  });
 }
 // --- SELECT ---
 
 
 //---PACIENTE---
-function consultarPaciente(){
-  var cedula=$('#cc').val();
+function consultarPaciente() {
+  var cedula = $('#cc').val();
   $.ajax({
     url: '/ajaxpaciente',
     type: 'POST',
     datatype: 'json',
-    data:{      
-      cedula:cedula  
+    data: {
+      cedula: cedula
     },
     success: (data) => {
       if (data[0]) {
-        paciente=data[0];
-        $('#nombres').val(data[0].nombre +" "+data[0].apellido+" "+data[0].sapellido);
+        paciente = data[0];
+        $('#nombres').val(data[0].nombre + " " + data[0].apellido + " " + data[0].sapellido);
         $('#edad').val(data[0].edad);
         selectEntidad();
-      }else{
+      } else {
         if (confirm("El Paciente no Existe en la Base de datos desea crearlo ? ")) {
           location.href = "/addpacientes";
         } else {
@@ -142,48 +142,48 @@ function consultarPaciente(){
         }
       }
     }
-  });  
+  });
 }
 
 
-function consultarEntidad(){
-  var nit=$('#entidad').val();
+function consultarEntidad() {
+  var nit = $('#entidad').val();
   $.ajax({
     url: '/ajaxentidad',
     type: 'POST',
     datatype: 'json',
-    data:{      
-      nit:nit  
+    data: {
+      nit: nit
     },
     success: (data) => {
-      eps=data[0];
-    
+      eps = data[0];
+
     }
-  });  
+  });
 }
-function consultarItem(){
-  var id=$('#item').val();
+function consultarItem() {
+  var id = $('#item').val();
   $.ajax({
     url: '/ajaxitems',
     type: 'POST',
     datatype: 'json',
-    data:{      
-      id:id  
+    data: {
+      id: id
     },
-    success: (data) => {      
+    success: (data) => {
       console.log(data[0]);
-      item=data[0];
+      item = data[0];
     }
-  });  
+  });
 }
 
-function additem() { 
-  var aut=$('#autorizacion').val();
-  if (aut!="" && $('item').val()!="") {
-    listitem.push({cups:item.cups,tarifa:item.valor,procedimiento:item.nombre,autorizacion:aut}); 
-    var cad="";
+function additem() {
+  var aut = $('#autorizacion').val();
+  if (aut != "" && $('item').val() != "") {
+    listitem.push({ cups: item.cups, tarifa: item.valor, procedimiento: item.nombre, autorizacion: aut });
+    var cad = "";
     listitem.forEach(element => {
-      cad+=`   
+      cad += `   
         <tr>
         <th>${element.procedimiento}</th>
         <td>${element.cups}</td>
@@ -191,63 +191,72 @@ function additem() {
         <td>${element.tarifa}</td>
         </tr>`;
     });
-     $('#res').html(cad);
-  }else{
+    $('#res').html(cad);
+  } else {
     alert('Debe llenar los campos ');
   }
 
 }
 
 function facturar() {
-  var consecutivo=parseInt($('#consecutivo').val());
-  var factura={
-    razon:'CARLOS PARRA BUSINESS MEDICAL CENTER SAS',
-    nit:'90098069-3',
-    habilitacion:'1300102937',
-    direccion:'',
-    telefonos:'6552095-3023513182',
-    email:'gerencia@carlosparra.co',
-    prefijo:'CP',
-    consecutivo:consecutivo,
-    paciente:paciente,
-    eps:eps,
-    items:listitem
+  diaActual = new Date();
+  var day = diaActual.getDate();
+  var month = diaActual.getMonth() + 1;
+  var year = diaActual.getFullYear();
+  if(parseInt(day)<10) day='0'+day;
+  if(parseInt(month)<10) month='0'+month;
+  fecha = day + '/' + month + '/' + year ;
+
+  var consecutivo = parseInt($('#consecutivo').val());
+  var factura = {
+    razon: 'CARLOS PARRA BUSINESS MEDICAL CENTER SAS',
+    nit: '90098069-3',
+    habilitacion: '1300102937',
+    direccion: '',
+    telefonos: '6552095-3023513182',
+    email: 'gerencia@carlosparra.co',
+    prefijo: 'CP',
+    fecha:fecha,
+    consecutivo: consecutivo,
+    paciente: paciente,
+    eps: eps,
+    items: listitem
   }
-  var fac= JSON.stringify(factura);
- 
-if($('#cc').val()!="" && $('#entidad').val()!="" && $('#autorizacion').val()!="" && listitem[0]){
-  $.ajax({
-    url: '/facturar',
-    type: 'POST',
-    datatype: 'json',
-    data:{      
-      fac:fac  
-    },
-    success: (data) => {      
-      console.log(data);
-      if (data=="ingresado") {
-        location.href = "/facturar";
+  var fac = JSON.stringify(factura);
+
+  if ($('#cc').val() != "" && $('#entidad').val() != "" && $('#autorizacion').val() != "" && listitem[0]) {
+    $.ajax({
+      url: '/facturar',
+      type: 'POST',
+      datatype: 'json',
+      data: {
+        fac: fac
+      },
+      success: (data) => {
+        console.log(data);
+        if (data == "ingresado") {
+          location.href = "/facturar";
+        }
+
       }
-     
-    }
-  }); 
-}else{
-  alert('Ha datos que no han sido agregados!');
-}
- 
- 
+    });
+  } else {
+    alert('Ha datos que no han sido agregados!');
+  }
+
+
 }
 
 function verConsecutivo() {
   $.ajax({
     url: '/consecutivo',
     type: 'POST',
-    datatype: 'json',    
-    success: (data) => {      
+    datatype: 'json',
+    success: (data) => {
       console.log(data);
-     $('#consecutivo').val(parseInt(data[0].cons)+1);
-     
+      $('#consecutivo').val(parseInt(data[0].cons) + 1);
+
     }
-  });  
-   
+  });
+
 }
--- src/routes/miempresa.js
+++ src/routes/miempresa.js
@@ -1,38 +1,88 @@
 const { Router } = require('express');
 const router = Router();
 var admin = require("firebase-admin");
-const db=admin.firestore();
+const db = admin.firestore();
 const fs = require('fs');
 
 
-router.get('/miempresas',(req,res)=>{   
+router.get('/miempresas', (req, res) => {
     let docRef = db.collection('carlosparra').doc();
     let setAda = docRef.set({
         nit: '111',
         rzocial: 'carlos parra',
-        habilitacion:'130000012',
-        direccion:'',
-        telfonos:'',
-        web:'',
-        email:'',
-        resolfac:''
+        habilitacion: '130000012',
+        direccion: '',
+        telfonos: '',
+        web: '',
+        email: '',
+        resolfac: ''
     });
     res.redirect('/');
 });
 
-router.get('/file',(req,res)=>{
-    fs.appendFile('file/ejemplo2.txt','Ejemplo texto\n',(error)=>{
+router.get('/file', (req, res) => {
+
+    var day = diaActual.getDate();
+    var month = diaActual.getMonth() + 1;
+    var year = diaActual.getFullYear();
+    fecha = day + '/' + month + '/' + year;
+
+
+    fs.appendFile('file/ejemplo2.txt', 'Ejemplo texto\n', (error) => {
         if (error) {
             throw error;
         }
-        res.download('file/ejemplo2.txt');
-
-        console.log('Archivos Creados'+__dirname);
     });
     res.redirect('/');
 });
 
-router.get('/descargas',(req,res)=>{  
+
+router.get('/facturips', (req, res) => {
+    db.collection('facturas').get()
+        .then((snapshot) => {
+            var valores = [];
+            snapshot.forEach((doc) => {
+                console.log(doc.id, '=>', doc.data());
+                valores.push(doc.data());
+                fs.appendFile('file/AF00001.txt', `${doc.data().habilitacion},${doc.data().razon},NI,${doc.data().nit},CP${doc.data().consecutivo},${doc.data().fecha}\n`, (error) => {
+                    if (error) {
+                        throw error;
+                    }
+                });
+            });
+            res.send(valores[0].eps.rsocial);
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+            res.send(valores);
+        });
+});
+
+router.get('/descargas', (req, res) => {
     res.download('file/ejemplo2.txt');
 });
+
+function conseRit(num) {
+    switch (num) {
+        case num < 10:
+            return '00000' + num;
+            break;
+        case num < 100:
+            return '0000' + num;
+            break;
+        case num < 1000:
+            return '000' + num;
+            break;
+        case num < 10000:
+            return '00' + num;
+            break;
+        case num < 100000:
+            return '0' + num;
+            break;
+        default:
+            return num;
+            break;
+    }
+}
+
 module.exports = router;
