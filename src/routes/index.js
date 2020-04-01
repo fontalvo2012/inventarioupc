d2d1f7609639a57857b940f09e459592dbd84c62
Steven Joseph Fontalvo Mora <programadoresudc@gmail.com>
2020-04-01 17:33:01 -0500

    cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 var admin = require("firebase-admin");
 
 const router = Router();
-var serviceAccount = require("../../gpsview-15f80-firebase-adminsdk-y4wtq-64941aee0f.json");
+//var serviceAccount = require("../../gpsview-15f80-firebase-adminsdk-y4wtq-64941aee0f.json");
+var serviceAccount = require("../../citas-orl-firebase-adminsdk-n4rdb-0454991b52.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://gpsview-15f80.firebaseio.com"
+ // databaseURL: "https://gpsview-15f80.firebaseio.com"
+  databaseURL: "https://citas-orl.firebaseio.com"
 });
 const db=admin.firestore();
 
